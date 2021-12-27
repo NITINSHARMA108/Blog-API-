d97140075fc93d4dcaf086fe9e7b0299ff22d3e5
nitin sharma <nitin.sharma@jalantechnologies.com>
2021-12-27 14:13:19 +0530

    PORT

--- app.js
+++ app.js
@@ -53,6 +53,6 @@ app.use((req, res, next) => {
 });
 
 app.use('/', routes);
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`listening to ${port}`));
